3d5bb1ac1912193e525212a2635ee2ca65c69324
HauntedCheppu <anupc8848@gmail.com>
2023-01-14 11:19:51 +0545

    get input from stdin

--- classobject.py
+++ classobject.py
@@ -1,11 +1,12 @@
 
+from sys import stdin
 
 
 class Person(  ):
     """docstring for Person."""
     def __init__(self, name, age):
-        self.age = age
         self.name = name
+        self.age =  age
         
     def show(self):
         print("my name is ", self.name)
@@ -17,29 +18,17 @@ class Person(  ):
     def get_school(self):
         return self.school
 
-demo_p1 = Person("kajol", 22)
-demo_p2 = Person("renuka", 25)
+# demo_p1 = Person("kajol", 22)
+# demo_p2 = Person("renuka", 25)
 
-demo_p2.set_school("Oxford")
+# demo_p2.set_school("Oxford")
 
-print(demo_p1.show())
-print(demo_p2.age)
+# print(demo_p1.show())
+# print(demo_p2.age)
 
-print(demo_p2.get_school())
+# print(demo_p2.get_school())
 
-
-#default customer
-
-print("Default customer name")
-class default_p( ):
-    """docstring for default_p."""
-    def __init__(self,   ):
-        self.name = "ram"
-        self.age = 27
-
-    def return_customer(self):
-        print( "name:", self.name, "\t age:", self.age)
-
-
-default_c = default_p()
-default_c.return_customer()
+''' Input from argument'''
+in_a = input("enter name and age ").partition(" ")
+inp_cus = Person(in_a[0], in_a[2])
+inp_cus.show()
