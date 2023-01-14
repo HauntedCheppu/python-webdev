f18847c4ad84143819a7b3f05442bb6760728872
HauntedCheppu <anupc8848@gmail.com>
2023-01-14 20:16:23 +0545

    update flask app

--- flaskapp.py
+++ flaskapp.py
@@ -1,1 +1,14 @@
 #! /bin//python
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+
+def hello(  ):
+    return '<h2> Welcome to Flask web app</h2>'
+
+
+if __name__ == '__main__':
+    app.run()
+
+
